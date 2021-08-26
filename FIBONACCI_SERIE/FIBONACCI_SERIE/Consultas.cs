8c56f2be50303dc9e0b0f000288ecc20d6329462
Cesar David <cesarakosta0@gmail.com>
2021-08-26 16:08:28 -0500

    Serie Fibonacci .Net

---
+++ FIBONACCI_SERIE/FIBONACCI_SERIE/Consultas.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIBONACCI_SERIE
+{
+    class Consultas
+    {
+        public int SerieFibonacci(int num)
+        {
+            if (num > 1)
+            {
+                return SerieFibonacci(num - 1) + (num - 2);
+            }
+            else if (num == 1)
+            {
+                return 1;
+            }
+            else if (num == 0)
+            {
+                return 0;
+            }
+            else
+            {
+              //  Console.WriteLine("el numero ingresado no es valido");
+                return -1;
+            }
+        }
+        public Boolean numprimo(int num)
+        {
+            Boolean numero = true;
+            int divisor = 2;
+            while ((numero) && (divisor != num))
+            {
+                if ((num % divisor == 0))
+                {
+                    numero = false;
+                }
+                else
+                {
+                    divisor++;
+                }
+            }
+            return numero;
+        }
+    }
+}
---
+++ FIBONACCI_SERIE/FIBONACCI_SERIE/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIBONACCI_SERIE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Consultas calculos = new Consultas();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("el numero segun la serie es: " + calculos.SerieFibonacci(i));
+
+                Console.WriteLine("el numero obtenido de la serie es primo: " + calculos.numprimo(i));
+
+            }
+            Console.ReadKey(true);
+
+        }
+    }
+}
